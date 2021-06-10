ab3bb2e270cc0f1ee54310cf1299081f5a9deed2
mOHITH998 <mohith98@gmail.com>
2021-06-10 15:39:59 +0530

    code updated

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,7 +3,12 @@ const { registerUser, loginUser } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router
+.route('/register')
+.post(registerUser);
+
+router
+.route('/login')
+.post(loginUser);
 
 module.exports = router;
