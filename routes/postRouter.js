9e4d5b9377c3b4eed4f336a1cfee32c377f22766
mOHITH998 <mohith98@gmail.com>
2021-06-10 15:41:45 +0530

    protect middleware is added

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const {
   getAllPosts,
+  getPosts,
   createPost,
   updatePost,
 } = require('../controllers/postController');
 
+const authConroller = require('../controllers/authController')
+
 const router = express.Router();
 
-router.get('/', getAllPosts);
-router.post('/post-create', createPost);
-router.put('/:id', updatePost);
+router
+.route('/')
+.get(authConroller.protect, getAllPosts);
+
+router
+.route('/:id')
+.get(authConroller.protect, getPosts);
+
+router
+.route('/post-create')
+.post(authConroller.protect, createPost);
+
+router
+.route('/:id')
+.put(authConroller.protect, updatePost);
 
 module.exports = router;
